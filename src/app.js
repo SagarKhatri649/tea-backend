9ae787d3596f25c8d706cedc80000bdcc1fb81e5
SagarKhatri649 <138390171+SagarKhatri649@users.noreply.github.com>
2026-09-01 21:24:29 +0530

    tweet controller and routes

--- src/app.js
+++ src/app.js
@@ -28,11 +28,32 @@ app.use(cookieParser())//server se user k browser ki cooki access kr paau or set
 // routes import
 
 import userRouter from './routes/user.routes.js' //Import user routes
+ 
+ 
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+ 
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+ 
 
 
 // routes declaration
 
 app.use("/api/v1/users",userRouter)     //Mount at /api/v1/users
+
+
+ 
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+ 
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+ 
+
 
 //ex- http://localhost:8000/api/v1/user/register
 
