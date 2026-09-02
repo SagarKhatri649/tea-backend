44e78d2c43a7a6adca8247190a4c3419a76e301f
SagarKhatri649 <138390171+SagarKhatri649@users.noreply.github.com>
2026-09-02 20:00:26 +0530

    Comment Controller

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ import videoRouter from "./routes/video.routes.js"
  
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
- 
+ import commentRouter from "./routes/comment.routes.js"
 
 
 // routes declaration
@@ -53,7 +53,7 @@ app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
  
-
+app.use("/api/v1/comments", commentRouter)
 
 //ex- http://localhost:8000/api/v1/user/register
 
